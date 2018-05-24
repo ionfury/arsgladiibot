9279d9f7401aee6ddc1565177201be9068e31061
Thomas Nowak <tnowak@plex.com>
2018-05-24 13:24:20 -0400

    basic bot

---
+++ app.js
@@ -0,0 +1,19 @@
+let Discord = require(`discord.js`);
+let Promise = require('bluebird');
+let Client = new Discord.Client();
+
+Client.on('ready', () => {
+  console.log(`\nBot has started, with ${Client.users.size} users, in ${Client.channels.size} channels of ${Client.guilds.size} guilds.`); 
+});
+
+Client.on('message', msg => {
+
+  if(msg.author.bot) return;
+  if(msg.content.toLowerCase().indexOf('poofy pants') !== 0) 
+    return;
+  else {
+    msg.reply(`${msg.author.tag} BANNED!`);
+  }
+});
+
+Client.login(process.env.token);
