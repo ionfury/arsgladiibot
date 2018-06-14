88eccf3478b03c873ec760d81bdc803ab5c2127d
Thomas Nowak <tnowak@plex.com>
2018-06-14 10:17:05 -0400

    testing on production

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ Client.on('message', msg => {
 Client.on('messageReactionAdd', (messageReaction, user) => {
   let emoji = messageReaction.emoji;
   let message = messageReaction.message;
-  console.log(emoji.identifier);
+  console.log(emoji.name);
 
   if(emoji.id == 1 /*:flag_us:*/)
   {
